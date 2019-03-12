ce077532a37e2499b728fb173cfc4ca7ae42ac17
Erik Peterson <erikeh1@gmail.com>
2019-03-12 13:16:09 -0400

    fixed small bug in conditional Sum file

--- conditionalSums.js
+++ conditionalSums.js
@@ -1,10 +1,18 @@
+////Attempt 1 USEING NESTED TERNARY ... COOOOOOL!
+
 let conditionalSum = function (values, condition) {
-  if (values.length) {
-    return condition === 'odd' ? values.filter(num => num % 2 !== 0).reduce((a, b) => a + b) : values.filter(num => num % 2 === 0).reduce((a, b) => a + b);
-  } else {
-    return 0;
-  }
-};
+  return (values.length) ? condition === 'odd' ? values.filter(num => num % 2 !== 0).reduce((a, b) => a + b) : values.filter(num => num % 2 === 0).reduce((a, b) => a + b) : 0;
+}
+
+////Attempt 2
+
+// let conditionalSum = function (values, condition) {
+//   if (values.length) {
+//     return condition === 'odd' ? values.filter(num => num % 2 !== 0).reduce((a, b) => a + b) : values.filter(num => num % 2 === 0).reduce((a, b) => a + b);
+//   } else {
+//     return 0;
+//   }
+// };
 
 
 // switch (condition) {
@@ -17,9 +25,3 @@ console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
 console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
 console.log(conditionalSum([], "odd"));
 
-
-///// attempt 2
-
-// let conditionalSum = function (values, condition) {
-//   return condition.length !== 0 ? condition === 'odd' ? values.filter(num => num % 2 !== 0).reduce((a, b) => a + b) : values.filter(num => num % 2 === 0).reduce((a, b) => a + b) : 0;
-// };
